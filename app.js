b4b7a2fd06e88177b2d357775dfac7919581f6ec
‘RSpidla’ <rspidla@gmail.com>
2019-02-24 02:58:17 +0000

    Http request setup for openweathermap.org api

--- app.js
+++ app.js
@@ -3,5 +3,25 @@
 searchButton.addEventListener('click', searchWeather);
 
 function searchWeather() {
-    console.log('Search City: ' + searchCity.value);
+    var cityName = searchCity.value;
+    if (cityName.trim().length == 0) {
+        return alert('Please enter a City Name');
+    }
+    var http = new XMLHttpRequest();
+    var apiKey = '';
+    var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+    var method = 'GET';
+
+    http.open(method, url);
+    http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+            var data = JSON.parse(http.responseText);
+        } else if (http.readyState === XMLHttpRequest.DONE) {
+            alert('Something went wrong!');
+        } 
+    }
 };
+
+
+
+
